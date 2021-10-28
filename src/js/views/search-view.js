c3c054122230042ab86b3a0d21a515ba3e346d14
unknown <shelite17@gmail.com>
2021-10-27 20:08:38 -0600

    Redesigned search results info

--- src/js/views/search-view.js
+++ src/js/views/search-view.js
@@ -2,42 +2,55 @@ import ejs from 'ejs';
 
 //view for when there are no results
 const noResultView = `<aside class="noResults">
-                        <h2>No results found</h2>
-                        </aside>`;
+                    <p>No results found</p>
+                    </aside>`;
 
 //view for when there are valid results
 const foundResultView = `<aside class="symbolInfo">
-                        <h2><%=ticker['01. symbol']%> Information</h2>
-                        <div class="info lineOne">
-                            <span class="symbol"><%=ticker['01. symbol']%></span>
-                            <span class="price"><%=ticker['05. price']%> USD</span>
+                    <div class="topInfo">
+                        <h2 class="symbol"><%=ticker['01. symbol']%></h2>
+                        <div class="divider"></div>
+                        <div class="info sectionOne">
+                            <p class="price"><%=ticker['05. price']%> USD</p>
+                            <p class="closeDate">Closed on <%=ticker['07. latest trading day']%></p>
                         </div>
-                        <div class="info lineTwo">
-                            <p>Closed <span class="date"><%=ticker['07. latest trading day']%></span></p>
+                    </div>
+                    <div class="info sectionTwo">
+                        <div class="boxOne">
+                            <p class="closeAmt"><%=ticker['08. previous close']%></p>
+                            <p>Previous close</p>
                         </div>
-                        <div class="info lineThree">
-                            <p>Previous close <span class="close"><%=ticker['08. previous close']%></span>, change of <span class="change"><%=ticker['09. change']%></span> <span class="change-per">(<%=ticker['10. change percent']%>)</span></p>
+                        <div class="boxTwo">
+                            <p class="changeValue"><%=ticker['09. change']%></p>
+                            <p>Change value</p>
                         </div>
-                        </aside>`;
+                        <div class="boxThree">
+                            <p class="changePercent"><%=ticker['10. change percent']%></p>
+                            <p>Change percentage</p>
+                        </div>
+                    </div>
+                </aside>`;
 
 function SearchView() {
 
     //assign the results aside
     this.container = document.querySelector('#results');
 
-    this.renderView = function(searchResult) {
+    this.renderView = function (searchResult) {
         //clear the results div before we render out the result
         this.removeChildElements();
-        
+
         const renderedElement = searchResult.then((ticker) => {
             //if no results, render the foundResultView, otherwise render the noResultView
-            if (ticker['01. symbol'] == undefined || ticker['01. symbol']  == null) {
+            if (ticker['01. symbol'] == undefined || ticker['01. symbol'] == null) {
                 const elem = ejs.render(noResultView);
                 //insert view
                 this.container.insertAdjacentHTML("afterbegin", elem);
             } else {
                 //must remember to add curly brace for object
-                const elem = ejs.render(foundResultView, {ticker});
+                const elem = ejs.render(foundResultView, {
+                    ticker
+                });
                 //insert view
                 this.container.insertAdjacentHTML("afterbegin", elem);
             }
@@ -47,7 +60,7 @@ function SearchView() {
     }
 
     //clear the results div
-    this.removeChildElements = function(){
+    this.removeChildElements = function () {
         this.container.querySelectorAll('aside').forEach((searchResult) => {
             this.container.removeChild(searchResult);
         })
